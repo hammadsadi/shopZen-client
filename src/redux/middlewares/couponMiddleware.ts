24f99b44c57f9cf9f0c13d28b9ad4c5fdf8f4287
hammadsadi <hammad.sadi@yahoo.com>
2025-02-28 09:19:28 +0600

    Multiple product discount functionality added using redux middleware

--- src/redux/middlewares/couponMiddleware.ts
+++ src/redux/middlewares/couponMiddleware.ts
@@ -1,3 +1,33 @@
 import { Action, Dispatch, Store } from "@reduxjs/toolkit";
+import {
+  addToCart,
+  clearCartItems,
+  decreamentCartItem,
+  fetchCoupon,
+  increamentCartItem,
+  subTotalSelectTor,
+} from "../features/cart/cartSlice";
+import { RootState } from "../store";
 
-export const couponMiddleware = (store: Store)=> (next:Dispatch)=> (action:Action)=>{}
+export const couponMiddleware =
+  (store: Store) => (next: Dispatch) => (action: Action) => {
+    if (
+      action.type === addToCart.type ||
+      action.type === increamentCartItem.type ||
+      action.type === decreamentCartItem.type ||
+      action.type === clearCartItems.type
+    ) {
+      next(action);
+      const state: RootState = store.getState();
+      const subTotal = subTotalSelectTor(state);
+      store.dispatch(
+        fetchCoupon({
+          shopId: state.cart.shopId,
+          couponCode: state.cart.coupon.code,
+          subTotal: subTotal,
+        }) as unknown as Action
+      );
+    } else {
+      next(action);
+    }
+  };
